5f36ebae928144d63f157f3213bbd12ebd4060fb
Alexander Huber <alih83@gmx.de>
2019-06-02 17:21:55 +0200

    add winston logging

--- imports/api/users/resolvers.js
+++ imports/api/users/resolvers.js
@@ -1,75 +1,110 @@
 import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
-import first from 'lodash/first';
+
+const { createLogger, transports, format } = require('winston');
+
+const { combine, timestamp, label, printf } = format;
+
+const loggerFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(label({ label: 'UserResolver' }), timestamp(), loggerFormat),
+  transports: [new transports.Console()],
+});
 
 export default {
   Query: {
-    currentUser(obj, args, context) {
+    currentUser(_, __, context) {
       const reqUser = context.user;
+      logger.log({ level: 'info', message: `got currentUser request for _id ${reqUser && reqUser._id}` });
       const user = reqUser && Meteor.users.findOne({ username: reqUser.username });
       if (user) {
+        logger.log({ level: 'info', message: `got currentUser with _id ${reqUser && reqUser._id}` });
         return user;
       } else {
+        logger.log({ level: 'info', message: 'currentUser not found, returning {}' });
         return {};
       }
     },
-    users(obj, args, context) {
+    users(_, __, context) {
       const user = context.user;
+      logger.log({ level: 'info', message: `got users request for _id ${user && user._id}` });
       if (!user.admin) {
+        logger.log({ level: 'warn', message: `users requester with _id ${user._id} is no admin` });
         return [];
       }
+      logger.log({ level: 'info', message: `returning users for _id ${user._id}` });
       return Meteor.users.find({}, { fields: { emails: 1, admin: 1, username: 1 } }).fetch();
     },
   },
   Mutation: {
-    createUser(obj, args, context) {
+    createUser(_, args, context) {
       const { username, admin, password } = args;
       const user = context.user;
+      logger.log({ level: 'info', message: `got createUser request from _id ${user && user._id}` });
       if (!user.admin) {
+        logger.log({ level: 'warn', message: `create user requester with _id ${user._id} is no admin` });
         throw new Error('not authorized');
       }
       const userId = Accounts.createUser({
         username,
         password,
       });
+      logger.log({ level: 'info', message: `created user with _id ${userId}` });
       if (admin) {
+        logger.log({ level: 'info', message: `setting admin flag on user with _id ${userId}` });
         Meteor.users.update({ _id: userId }, { $set: { admin } });
       }
-      return first(Meteor.users.find({ _id: userId }, { fields: { admin: 1, username: 1 } }).fetch());
+      const newUser = Meteor.users.findOne({ _id: userId }, { fields: { admin: 1, username: 1 } });
+      logger.log({ level: 'info', message: `created user ${JSON.stringify(newUser)}` });
+      return newUser;
     },
-    updateUser(obj, args, context) {
+    updateUser(_, args, context) {
       const { userId, username, admin, password } = args;
       const adminBefore = Meteor.users.findOne(userId).admin;
       const user = context.user;
+      logger.log({ level: 'info', message: `got updateUser request from _id ${user && user._id}` });
       if (!user.admin) {
+        logger.log({ level: 'warn', message: `update user requester with ${user._id} is no admin` });
         throw new Error('not authorized');
       }
+      logger.log({ level: 'info', message: `updating user with _id ${userId}, setting admin to ${admin}` });
       Meteor.users.update({ _id: userId }, { $set: { username, admin } });
       if (password.length >= 8) {
+        logger.log({ level: 'info', message: `setting new password for user with _id ${userId}` });
         Accounts.setPassword(userId, password);
       }
       if (admin !== adminBefore) {
+        logger.log({ level: 'info', message: `forcing user with _id ${userId} to logout since he became admin` });
         Meteor.users.update({ _id: userId }, { $set: { 'services.resume.loginTokens': [] } });
       }
+      logger.log({ level: 'info', message: `updated user with _id ${userId}` });
       return Meteor.users.findOne({ _id: userId }, { fields: { admin: 1, username: 1 } });
     },
-    deleteUser(obj, args, context) {
+    deleteUser(_, args, context) {
       const { userId } = args;
       const user = context.user;
+      logger.log({ level: 'info', message: `got deleteUser request from _id ${user && user._id}` });
       if (!user.admin) {
+        logger.log({ level: 'warn', message: `delete user requester with _id ${user._id} is no admin` });
         throw new Error('not authorized');
       }
       const userToDelete = Meteor.users.findOne({ _id: userId });
       if (userToDelete) {
+        logger.log({ level: 'info', message: `deleting user with _id ${userId}` });
         try {
           Meteor.users.update({ _id: userId }, { $set: { 'services.resume.loginTokens': [] } });
           Meteor.users.remove({ _id: userId });
+          logger.log({ level: 'info', message: `deleted user with _id ${userId}` });
           return true;
         } catch (err) {
-          console.log(err);
+          logger.log({ level: 'err', message: `deleting user with _id ${userId} failed: ${JSON.stringify(err)}` });
           return false;
         }
       } else {
+        logger.log({ level: 'info', message: `could not delete user with _id ${userId}` });
         return false;
       }
     },
--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,9 +1,23 @@
+import { Accounts } from 'meteor/accounts-base';
 import { ApolloServer } from 'apollo-server-express';
 import { WebApp } from 'meteor/webapp';
 import { getUser } from 'meteor/apollo';
 import merge from 'lodash/merge';
 import UserSchema from '../../api/users/User.graphql';
 import UserResolver from '../../api/users/resolvers';
+
+const { createLogger, transports, format } = require('winston');
+
+const { combine, timestamp, label, printf } = format;
+
+const loggerFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(label({ label: 'MeteorServer' }), timestamp(), loggerFormat),
+  transports: [new transports.Console()],
+});
 
 const typeDefs = [UserSchema];
 
@@ -26,4 +40,12 @@ WebApp.connectHandlers.use('/graphql', (req, res) => {
   if (req.method === 'GET') {
     res.end();
   }
+});
+
+Accounts.onLogin((loginObj) => {
+  logger.info({ level: 'info', message: `successful login for user ${loginObj.user.username} with _id ${loginObj.user._id}` });
+});
+
+Accounts.onLogout((logoutObj) => {
+  logger.info({ level: 'info', message: `successful logout for user ${logoutObj.user.username} with _id ${logoutObj.user._id}` });
 });
