9d186dba06ed1a50dc9eaecf7e9041f1f5c9093b
Alexander Huber <alih83@gmx.de>
2019-12-16 19:09:11 +0100

    add basic settings integration spec

---
+++ tests/cypress/integration/settings_spec.js
@@ -0,0 +1,37 @@
+/* globals cy expect */
+
+describe('user settings', () => {
+  before(() => {
+    cy.resetDatabase();
+    cy.seedUsers();
+    cy.seedSettings();
+  });
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should save seeded settings', () => {
+    cy.contains('Login').click();
+    cy.get('input[name=username]').type('testuser');
+    cy.get('input[name=password]').type('testuser');
+    cy.get('input[type=submit]').click();
+
+    cy.url().should('eq', 'http://localhost:3000/');
+
+    cy.window().then(win => {
+      // this allows accessing the window object within the browser
+      const user = win.Meteor.user();
+      expect(user).to.exist;
+      expect(user.username).to.equal('testuser');
+      cy.contains('Menu').click();
+      cy.contains('Settings').click();
+
+      cy.window().then(win2 => {
+        cy.url().should('eq', 'http://localhost:3000/settings');
+        cy.get('input[type=submit]').click();
+        cy.get('body').should('contain', 'Update successful!');
+      });
+    });
+  });
+});
--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -1,6 +1,14 @@
 /* globals Cypress cy */
 Cypress.Commands.add('resetDatabase', () => cy.exec('~/mongodb4/bin/mongo mongodb://localhost:3001/meteor --eval "db.dropDatabase()"'));
 
-Cypress.Commands.add('seedUsers', () => cy.exec(`~/mongodb4/bin/mongo mongodb://localhost:3001/meteor ./${process.cwd()}tests/cypress/support/seedUsers`));
+Cypress.Commands.add('seedUsers', () =>
+  cy.exec(`~/mongodb4/bin/mongo mongodb://localhost:3001/meteor ./${process.cwd()}tests/cypress/support/seedUsers`)
+);
 
-Cypress.Commands.add('seedPaginateUsers', () => cy.exec(`~/mongodb4/bin/mongo mongodb://localhost:3001/meteor ./${process.cwd()}tests/cypress/support/seedPaginateUsers`));
+Cypress.Commands.add('seedSettings', () =>
+  cy.exec(`~/mongodb4/bin/mongo mongodb://localhost:3001/meteor ./${process.cwd()}tests/cypress/support/seedSettings`)
+);
+
+Cypress.Commands.add('seedPaginateUsers', () =>
+  cy.exec(`~/mongodb4/bin/mongo mongodb://localhost:3001/meteor ./${process.cwd()}tests/cypress/support/seedPaginateUsers`)
+);
