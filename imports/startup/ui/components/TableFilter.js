518150d2ee98a0f812fe535f89b3ccd9e94fd0c5
Alexander Huber <alih83@gmx.de>
2020-08-18 20:45:39 +0200

    fix table filter popup appear on empty table, cleanups

--- imports/startup/ui/components/TableFilter.js
+++ imports/startup/ui/components/TableFilter.js
@@ -18,16 +18,18 @@ class TableFilter extends React.Component {
       this.setState({ [name]: value, filterValid: false });
     } else {
       this.setState({ [name]: value, filterValid: true });
-      this.props.onSubmitFilter(value);
+      const { onSubmitFilter } = this.props;
+      onSubmitFilter(value);
     }
   };
 
   render() {
-    const { filter } = this.state;
+    const { filter, filterValid } = this.state;
+    const { totalCount, loading } = this.props;
     let popupMessage = '';
-    if (!this.state.filterValid) {
+    if (!filterValid) {
       popupMessage = 'Invalid character.';
-    } else if (this.props.totalCount === 0) {
+    } else if (filter.length !== 0 && totalCount === 0) {
       popupMessage = 'No results found.';
     }
 
@@ -41,16 +43,16 @@ class TableFilter extends React.Component {
                   placeholder="Enter the filter."
                   name="filter"
                   value={filter}
-                  error={!this.state.filterValid}
+                  error={!filterValid}
                   label="Filter"
                   onChange={this.handleOnChange}
                   icon="search"
-                  loading={this.props.loading}
+                  loading={loading}
                 />
               )}
               content={popupMessage}
               on="click"
-              open={!this.state.filterValid || this.props.totalCount === 0}
+              open={filter.length !== 0 && (!filterValid || totalCount === 0)}
               position="right center"
             />
           </Form.Field>
